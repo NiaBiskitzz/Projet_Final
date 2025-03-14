83bb91bd205ecc57bea7103f372a4c20bc8f6114
NiaBiskitzz <wenwenwen2005@gmail.com>
2025-03-14 16:38:20 -0400

    Update fonction_aléatoire.py
    Choice caused problem because removes removed all the variable from the list
    
    Traceback (most recent call last):
      File "c:\Users\wenwe\OneDrive\Desktop\Projet_Final\Core\Aléatoire\fonction_aléatoire.py", line 62, in <module>
        print(aléatoire())
              ~~~~~~~~~^^
      File "c:\Users\wenwe\OneDrive\Desktop\Projet_Final\Core\Aléatoire\fonction_aléatoire.py", line 24, in aléatoire
        Random_game=random.choice(liste_jeux)
      File "C:\Users\wenwe\AppData\Local\Programs\Python\Python313\Lib\random.py", line 351, in choice
        raise IndexError('Cannot choose from an empty sequence')
    IndexError: Cannot choose from an empty sequence

--- Core/Aléatoire/fonction_aléatoire.py
+++ Core/Aléatoire/fonction_aléatoire.py
@@ -1,31 +1,12 @@
 # Fonction aléatoire
 
 liste_jeux=[1,2,3,4,5,6]
-bool(Jeu1_joué)=False
-bool(Jeu2_joué)=False
-bool(Jeu3_joué)=False
-bool(Jeu4_joué)=False
-bool(Jeu5_joué)=False
-bool(Jeu6_joué)=False
-if bool(Jeu1_joué)==True:
-    liste_jeux.remove(1)
-if bool(Jeu2_joué)==True:
-    liste_jeux.remove(2)
-if bool(Jeu3_joué)==True:
-    liste_jeux.remove(3)
-if bool(Jeu4_joué)==True:
-    liste_jeux.remove(4)
-if bool(Jeu5_joué)==True:
-    liste_jeux.remove(5)
-if bool(Jeu6_joué)==True:
-    liste_jeux.remove(6)
-
-import Jeu1
-import Jeu2
-import Jeu3
-import Jeu4
-import Jeu5
-import Jeu6
+Jeu1_joué=False
+Jeu2_joué=False
+Jeu3_joué=False
+Jeu4_joué=False
+Jeu5_joué=False
+Jeu6_joué=False
 
 def aléatoire():
     """
@@ -33,6 +14,8 @@ def aléatoire():
 
     Paramètres
     ----------
+    import random
+    random_game=random
 
     """
     
@@ -40,26 +23,40 @@ def aléatoire():
     import random
     Random_game=random.choice(liste_jeux)
     if Random_game==1:
-        bool(Jeu1_joué)=True
+        Jeu1_joué=True
         print("Jeu 1")
+        liste_jeux.remove(1)
         return 1
     elif Random_game==2:
-        bool(Jeu2_joué)=True
+        Jeu2_joué=True
         print("Jeu 2")
+        liste_jeux.remove(2)
         return 2
     elif Random_game==3:
-        bool(Jeu3_joué)=True
+        Jeu3_joué=True
         print("Jeu 3")
+        liste_jeux.remove(3)
         return 3
     elif Random_game==4:
-        bool(Jeu4_joué)=True
+        Jeu4_joué=True
         print("Jeu 4")
+        liste_jeux.remove(4)
         return 4
     elif Random_game==5:
-        bool(Jeu5_joué)=True
+        Jeu5_joué=True
         print("Jeu 5")
+        liste_jeux.remove(5)
         return 5
     elif Random_game==6:
-        bool(Jeu6_joué)=True
+        Jeu6_joué=True
         print("Jeu 6")
+        liste_jeux.remove(6)
         return 6
+
+print(aléatoire())
+print(aléatoire())
+print(aléatoire())
+print(aléatoire())
+print(aléatoire())
+print(aléatoire())
+print(aléatoire())
