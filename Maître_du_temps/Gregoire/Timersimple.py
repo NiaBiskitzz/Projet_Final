f83054e3d1ce81c06210232473f6ad2d7e6b4a6b
Grégoire Lemelin <202492084@cegepsherbrooke.qc.ca>
2025-03-14 15:04:18 -0400

    Update Timersimple.py

--- Maître_du_temps/Gregoire/Timersimple.py
+++ Maître_du_temps/Gregoire/Timersimple.py
@@ -1,6 +1,7 @@
 #permet d'utiliser la classe Timer facilement   
+from timeit import default_timer as Timer
 timer = Timer()
  
 # Votre code se trouve ici
  
-print(f"Programme exécuté en : {timer.get_time(): .5f} secondes")
+print(f"Programme exécuté en : {timer(): .5f} secondes")
