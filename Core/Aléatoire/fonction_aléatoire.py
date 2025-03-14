6537b02e3e759ce55ec16ac98507cbf0b109024f
NiaBiskitzz <wenwenwen2005@gmail.com>
2025-03-14 15:27:06 -0400

    Update fonction_aléatoire.py

--- Core/Aléatoire/fonction_aléatoire.py
+++ Core/Aléatoire/fonction_aléatoire.py
@@ -7,5 +7,35 @@ def aléatoire():
     Paramètres
         joué==False
         import random
+        Random_game=random.randint(1,6)
+            if Random_game==1:
+                bool(Jeu1_joué)=True
+                print("Jeu 1")
+                import Jeu1
+                Jeu1()
+            elif Random_game==2:
+                bool(Jeu2_joué)=True
+                print("Jeu 2")
+                import Jeu2
+                Jeu2()
+            elif Random_game==3:
+                bool(Jeu3_joué)=True
+                print("Jeu 3")
+                import Jeu3
+                Jeu3()
+            elif Random_game==4:
+                bool(Jeu4_joué)=True
+                print("Jeu 4")
+                import Jeu4
+                Jeu4()
+            elif Random_game==5:
+                bool(Jeu5_joué)=True
+                print("Jeu 5")
+                import Jeu5
+                Jeu5()
+            elif Random_game==6:
+                bool(Jeu6_joué)=True
+                print("Jeu 6")
+                import Jeu6
         
     """
