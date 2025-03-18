f3b8f7e8a5ba32186ff932c0dce3637f662e52a8
NiaBiskitzz <wenwenwen2005@gmail.com>
2025-03-18 17:17:20 -0400

    Changed .pop to .remove

--- Core/Aléatoire/fonction_aléatoire.py
+++ Core/Aléatoire/fonction_aléatoire.py
@@ -11,32 +11,32 @@ def aléatoire():
     import random
 
     """
-    
+
     joué=False
     import random
     Random_game=random.choice(liste_jeux)
     if Random_game==1:
-        liste_jeux.pop(1)
+        liste_jeux.remove(1)
         print("Jeu 1")
         return 1
     elif Random_game==2:
-        liste_jeux.pop(2)
+        liste_jeux.remove(2)
         print("Jeu 2")
         return 2
     elif Random_game==3:
-        liste_jeux.pop(3)
+        liste_jeux.remove(3)
         print("Jeu 3")
         return 3
     elif Random_game==4:
-        liste_jeux.pop(4)
+        liste_jeux.remove(4)
         print("Jeu 4")
         return 4
     elif Random_game==5:
-        liste_jeux.pop(5)
+        liste_jeux.remove(5)
         print("Jeu 5")
         return 5
     elif Random_game==6:
-        liste_jeux.pop(6)
+        liste_jeux.remove(6)
         print("Jeu 6")
         return 6
 
