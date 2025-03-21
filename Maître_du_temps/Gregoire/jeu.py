11e87ec0a2a5aa9864433033074158c44e081f7a
Grégoire Lemelin <202492084@cegepsherbrooke.qc.ca>
2025-03-21 14:43:19 -0400

    Update jeu.py

--- Maître_du_temps/Gregoire/jeu.py
+++ Maître_du_temps/Gregoire/jeu.py
@@ -8,3 +8,17 @@
 import time
 import datetime
 import random
+import countdown
+
+print (countdown.countdown(0, 10, 0))
+
+
+Questions = {
+    "Quel est le nom du pere de thor?": "Odin",
+    "Quel était le nom de New York entre 1624 et 1664 ?:" : "New Amsterdam",
+    "D'où vien SuperMan?": "Krypton",
+    "Qui est réellement Batman?": "Bruce Wayne",
+    "Quel est le nom du chien de Mickey Mouse?": "Pluto",
+    "Quel est le nom du premier film de Star Wars?": "A New Hope",
+}
+
