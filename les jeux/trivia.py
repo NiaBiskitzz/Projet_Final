42db2884c315a2b8a9cec4af45b75143089bbcc2
Grégoire Lemelin <202492084@cegepsherbrooke.qc.ca>
2025-03-21 15:28:30 -0400

    Update trivia.py

--- les jeux/trivia.py
+++ les jeux/trivia.py
@@ -8,19 +8,20 @@ si egalite, roche papier ciseaux"""
 import time
 import datetime
 import random
-from countdown import countdown
+import countdown # import le module pour le timer
 
-input("Voulez vous savoir comment jouer?")
+input("Voulez vous savoir comment jouer?") # demande si l<utilisateur sait jouer
 if input == "oui":
     print("Le but du jeu est de repondre correctement au plus de question possible")
 else:
     print("D'accord, commencons le jeu")
+print(countdown.countdown(0, 0, 0))
 
 
 
 
 
-Questions = {
+Questions = { #question qui vont etre posées
     "Quel est le nom du pere de thor?": "Odin",
     "Quel était le nom de New York entre 1624 et 1664 ?:" : "New Amsterdam",
     "D'où vien SuperMan?": "Krypton",
@@ -29,7 +30,7 @@ Questions = {
     "Quel est le nom du premier film de Star Wars?": "A New Hope",
 }
 
-def ask_questionJ1(Question):
+def ask_questionJ1(Question):# fonction pour poser les questions
     print(Question)
     reponse = input("Reponse: ")
     return reponse
