fd4a57dff33b70651ba8ae23dbc62bb6e313789c
Daphnee72 <daphneepelletier18@icloud.com>
2025-03-21 14:36:54 -0400

    Update Erreur.py
    modification et regler des chose

--- Affichage/Erreur.py
+++ Affichage/Erreur.py
@@ -2,27 +2,33 @@ def game_over(game):
     pointage_joueur1:playeur1
     pointage_joueur2:playeur2
 
-    if pointage_joueur1 == False:
-        print("________________")
-        print("|              |")
-        print("|    PERDU!    |")
-        print("|______________|")
-    elif pointage_joueur1 == True:
+    if  pointage_joueur1 == True:
+        pointage_joueur2 = not(pointage_joueur1)
+        print("    Playeur 1   ")
         print("________________")
         print("|              |")
         print("|   +1 point   |")
         print("|______________|")
+        print("   Playeur 2    ")
+        print("________________")
+        print("|              |")
+        print("|    PERDU!    |")
+        print("|______________|")
+    if  pointage_joueur1 == False:
+        pointage_joueur2 = not(pointage_joueur1)
+        print("    Playeur 1   ")
+        print("________________")
+        print("|              |")
+        print("|    PERDU!    |")
+        print("|______________|")
+        print("   Playeur 2    ")
+        print("________________")
+        print("|              |")
+        print("|   +1 point   |")
+        print("|______________|")
+       
 
-    if pointage_joueur2 == True:
-        print("________________")
-        print("|              |")
-        print("|   +1 point   |")
-        print("|______________|")
-    elif pointage_joueur2 == False: 
-        print("________________")
-        print("|              |")
-        print("|    PERDU!    |")
-        print("|______________|")
+game_over(True, False)
 
 
 
