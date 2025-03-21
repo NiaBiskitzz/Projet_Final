85a2f285c07a0bc2c6a979637fc67da02c7f23c0
Daphnee72 <daphneepelletier18@icloud.com>
2025-03-21 14:39:45 -0400

    Update Erreur.py
    commentaire

--- Affichage/Erreur.py
+++ Affichage/Erreur.py
@@ -2,10 +2,10 @@ def game_over(game):
     pointage_joueur1:playeur1
     pointage_joueur2:playeur2
 
-    if  pointage_joueur1 == True:
-        pointage_joueur2 = not(pointage_joueur1)
+    if  pointage_joueur1 == True: #si la donnée du joueur 1 est vrai sa veut dire qu'il obtient 1 point supplementaire et le joueur 2
+        pointage_joueur2 = not(pointage_joueur1)#perd cepandent il ne perd pas de point.
         print("    Playeur 1   ")
-        print("________________")
+        print("________________") 
         print("|              |")
         print("|   +1 point   |")
         print("|______________|")
