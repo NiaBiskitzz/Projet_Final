1b051e14cc1bebc0bc4a87bbf0fb0ca442063362
amelies08 <amelie.sarrazin2@gmail.com>
2025-03-21 14:41:40 -0400

    Progression des points

--- Core/Chargement/Debut_Jeu.py
+++ Core/Chargement/Debut_Jeu.py
@@ -1,6 +1,8 @@
+import keyboard
+
 def DebutJeu():
     """
-    Lorsque cette touche est appuyée, le jeu commence
+    Lorsque la touche espace est appuyée, le jeu commence
     """
     event = keyboard.read_event()
     if event.event_type == keyboard.KEY_DOWN and event.name == 'space' :
