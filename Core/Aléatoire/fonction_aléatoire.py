8f699d7dccd457e26f5a2cdc0d48c362ef1d84c5
NiaBiskitzz <wenwenwen2005@gmail.com>
2025-03-21 13:59:11 -0400

    comments added for Aléatoire

--- Core/Aléatoire/fonction_aléatoire.py
+++ Core/Aléatoire/fonction_aléatoire.py
@@ -9,15 +9,39 @@ def aléatoire():
     Paramètres
     ----------
     import random
-
-    """
-
-    joué=False
-    import random
-    Random_game=random.choice(liste_jeux)
-    if Random_game==1:
+    random_game=random.choice(liste_jeux)à
+    if random_game==1:
         liste_jeux.remove(1)
         print("Jeu 1")
+        return 1
+    elif random_game==2:
+        liste_jeux.remove(2)
+        print("Jeu 2")
+        return 2
+    elif random_game==3:
+        liste_jeux.remove(3)
+        print("Jeu 3")
+        return 3
+    elif random_game==4:
+        liste_jeux.remove(4)
+        print("Jeu 4")
+        return 4
+    elif random_game==5:
+        liste_jeux.remove(5)
+        print("Jeu 5")
+        return 5
+    elif random_game==6:
+        liste_jeux.remove(6)
+        print("Jeu 6")
+        return 6
+    """
+
+    import random   # Import random library.
+    Random_game=random.choice(liste_jeux)   # Choose a random game from the list.
+    # conditions and returns while making sure the option is removed from the list.
+    if Random_game==1:
+        liste_jeux.remove(1)
+        print("Jeu 1")  # Temporary print for testing, change to name of the game when testing the completed programme, also optionnal.
         return 1
     elif Random_game==2:
         liste_jeux.remove(2)
@@ -40,6 +64,7 @@ def aléatoire():
         print("Jeu 6")
         return 6
 
+# Tests (optionnal), Remove when done.
 print(aléatoire())
 print(aléatoire())
 print(aléatoire())
