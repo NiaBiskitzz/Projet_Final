272b0515239e025003cb87a274045ea33f035ed6
Antoine690 <202272651@cegepsherbrooke.qc.ca>
2025-03-28 15:09:46 -0400

    Update Réponse_finale.py

--- Core/Pointage/Réponse_finale.py
+++ Core/Pointage/Réponse_finale.py
@@ -1,18 +1,19 @@
 #si le joueur 1 gagne = vrai et joueur 2 gagne = faux
-
-#player1 = True
-#player2 = False
 joueur1 = input()
 joueur2 = input()
 pointage_joueur1 = 0
 pointage_joueur2 = 0
-if joueur1:
-    resultat_mini_jeux = True
-elif joueur2:
-    resultat_mini_jeux = False
-#Si le joueur 1 a un resultats plus grand que le joueur 2, le joueur 1 gagne
-if pointage_joueur1 > pointage_joueur2:
-    resultat_mini_jeux = True
+#player1 = True
+#player2 = False
+def banque(pointage_joueur1, pointage_joueur2, resultat_mini_jeux):
+    #Si le joueur 1 a un resultats plus grand que le joueur 2, le joueur 1 gagne
+    if pointage_joueur1 > pointage_joueur2:
+        resultat_mini_jeux = True
+    if resultat_mini_jeux == True:
+        pointage_joueur1 += 1
     #Si le joueur 2 a un resultats plus grand que le joueur 1, le joueur 2 gagne
-elif pointage_joueur1 < pointage_joueur2:
-    resultat_mini_jeux = False
+    elif pointage_joueur1 < pointage_joueur2:
+        resultat_mini_jeux = False
+    elif resultat_mini_jeux == False:
+        pointage_joueur2 += 1
+    return pointage_joueur1, pointage_joueur2
