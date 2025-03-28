3981023f48da772dba46273b42c99592bc17ae28
broute2 <charles.brouillette100@gmail.com>
2025-03-28 13:52:24 -0400

    mise à jour
    oui

---
+++ Maître_du_temps/Amé/Untitled-1.py
@@ -0,0 +1,9 @@
+print("1ère manche")
+passer_règle_1m= input("voulez vous avoir avoir les réglement de la manche? (yes or no)")
+if passer_règle_1m == "yes":
+    print("yes")
+else:
+        print("tabarnaque")
+
+
+    
