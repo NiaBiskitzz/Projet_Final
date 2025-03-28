63023941f4b4778e54e2ec5ae472843fa1fdfa75
Grégoire Lemelin <202492084@cegepsherbrooke.qc.ca>
2025-03-28 16:11:57 -0400

    Update trivia quest.py

--- trivia quest.py
+++ trivia quest.py
@@ -1,81 +1,84 @@
-import random
-#import countdown# marche pas, il faut que le code roule pendant le countdown
 
-def trivia_game1():
-    questions1 = [
-        {"question": "Quelle est la capitale de la France ?", "reponse": "Paris"},
-        {"question": "Combien de continents y a-t-il ?", "reponse": "7"},
-        {"question": "Quelle planète est connue comme la planète rouge ?", "reponse": "Mars"},
-        {"question": "Quelle est la racine carrée de 81 ?", "reponse": "9"},
-        {"question": "Quel est le plus grand océan sur Terre ?", "reponse": "Pacifique"}
-    ]
+from random import random
+from random import randint
+Films = {
+    "Quel est le nom du pere de thor?": "Odin",
+    "Quel était le nom de New York entre 1624 et 1664 ?": "New Amsterdam",
+    "D'où vien SuperMan?": "Krypton",
+    "Qui est réellement Batman?": "Bruce Wayne",
+    "Quel est le nom du chien de Mickey Mouse?": "Pluto",
+    "Quel est le nom du premier film de Star Wars?": "A New Hope"
+}
 
-def ask_questions1(player):
-        print(f"\n{player}, c'est ton tour !")
-        score = 0
-        for q in questions1:
-            user_reponse = input(q["question"] + " ")
-            if user_reponse.lower() == q["reponse"].lower():
-                print("Bonne réponse !")
-                score += 1
-            else:
-                print(f"Mauvaise réponse ! La bonne réponse était : {q['reponse']}")
-        return score
+Geographie = {"Quel pays possède le plus grand nombre d’îles dans le monde?": "Suède", "Quel est le plus petit pays du monde?": "Vatican", "Quel est le plus petit pays du monde?": "Vatican", "Quel est la capital du Canada": " Ottawa", "Citez le nom de la plus vaste chaine de montagne": "les Andes", "Citez le nom du plus long fleuve du monde": "le Nil", "Quel est le site naturel le plus bas de la planete terre": "la fosse de Mariannes"}
 
 
-def trivia_game2():
-    questions2 = [
-        {"question": "Quel est le nom du pere de thor?", "reponse": "Odin"},
-        {"question": "Quel était le nom de New York entre 1624 et 1664 ?", "reponse": "New Amsterdam"},
-        {"question": "D'où vien SuperMan?", "reponse": "Krypton"},
-        {"question": "Qui est réellement Batman?", "reponse": "Bruce Wayne"},
-        {"question": "Quel est le nom du chien de Mickey Mouse?", "reponse": "Pluto"},
-        {"question": "Quel est le nom du premier film de Star Wars?", "reponse": "A New Hope"}
-    ]
+kings_and_queens_of_rock = { "Who is known as the 'King of Rock and Roll'?": "Elvis Presley", "Who is known as the 'Queen of Rock and Roll'?": "Tina Turner", "Who is known as the 'Godfather of Soul'?": "James Brown", "Who is known as the 'King of the Blues'?": "B.B. King", "Who is known as the 'Queen of Soul'?": "Aretha Franklin", "Who is known as the 'King of Pop'?": "Michael Jackson", "Who is known as the 'Queen of Pop'?": "Madonna", "Who is known as the 'King of Punk'?": "Iggy Pop", "Who is known as the 'Queen of Punk'?": "Patti Smith", "Who is known as the 'King of Metal'?": "Ozzy Osbourne", "Who is known as the 'Queen of Metal'?": "Lita Ford", "Who is known as the 'King of Rockabilly'?": "Elvis Presley", "Who is known as the 'Queen of Rockabilly'?": "Wanda Jackson", "Who is known as the 'King of Country'?": "George Strait",}
 
-    def ask_questions2(player):
-        print(f"\n{player}, c'est ton tour !")
-        score = 0
-        for q in questions2:
-            user_reponse = input(q["question"] + " ")
-            if user_reponse.lower() == q["reponse"].lower():
-                print("Bonne réponse !")
-                score += 1
-            else:
-                print(f"Mauvaise réponse ! La bonne réponse était : {q['reponse']}")
-        return score
+def switch_player(current_player, player_count):
+    if player_count == 3:
+        if current_player < 3:
+            new_player = current_player + 1
+        else:
+            new_player = 1
+    if player_count == 2:
+        if current_player == 1:
+            new_player = 2
+        else:
+            new_player = 1
+    if player_count == 1:
+        new_player = 1
+    
+    return new_player
 
-    player1 = input("Nom du joueur 1 : ")
-    player2 = input("Nom du joueur 2 : ")
 
-    score1 = ask_questions1(player1)
-    score2 = ask_questions2(player2)
 
-    print(f"\nRésultats : {player1} a {score1} points, {player2} a {score2} points.")
-
-    if score1 > score2:
-        print(f"{player1} gagne !")
-    elif score2 > score1:
-        print(f"{player2} gagne !")
+game_theme = input("Select 1 pour Films, 2 pour Geographie, 3 pour Kings and Queens of Rock!  ")
+print (game_theme)
+game_data = {}
+if game_theme == "1":
+    game_data = Films
+else:
+    if game_theme == "2":
+        game_data = Geographie
     else:
-        print("Égalité ! C'est l'heure de Roche-Papier-Ciseaux.")
+        if game_theme == "3":
+            game_data = kings_and_queens_of_rock
+        else:
+            print("mauvaise selection, reesayer")
 
-        def roche_papier_ciseaux():
-            choices = ["Roche", "Papier", "Ciseaux"]
-            print("Choisissez : Roche, Papier ou Ciseaux.")
-            p1_choice = input(f"{player1}, fais ton choix : ")
-            p2_choice = input(f"{player2}, fais ton choix : ")
-            if p1_choice == p2_choice:
-                print("Égalité, rejouez !")
-                roche_papier_ciseaux()
-            elif (p1_choice == "Roche" and p2_choice == "Ciseaux") or \
-                 (p1_choice == "Papier" and p2_choice == "Roche") or \
-                 (p1_choice == "Ciseaux" and p2_choice == "Papier"):
-                print(f"{player1} gagne avec {p1_choice} contre {p2_choice} !")
-            else:
-                print(f"{player2} gagne avec {p2_choice} contre {p1_choice} !")
+game_length = len(game_data)
+fixed_game_length = game_length
+counter = 1
+scores = [0, 0, 0]
+questions = list(game_data)
 
-        roche_papier_ciseaux()
+player_count = input("combien de joueur? (1 , 2, or 3)")
 
-# Démarrer le jeu
-trivia_game1()
+if player_count != "1" and player_count != "2" and player_count !="3":
+    print ("entre invalide - reesayer")
+    player_count = 0
+    counter = 100
+
+player_num = 1
+while counter <= fixed_game_length:
+    print ("Ok " + "player " + str(player_num) + " c'est ton tour.  Answer question " + str(counter) + " of " + str(fixed_game_length))
+    question_num = randint(1, game_length) - 1
+    question = questions[question_num]
+    answer_question = input (question)
+    if answer_question == game_data.get(question):
+        scores [player_num-1] += 1
+        print ("Correct! " + "vous avez fait un point. " + "tu a " + str(scores[player_num-1]) + " points.")
+    else: 
+        print ("mauvais - la bonne reponse est " + game_data.get(question))
+    counter += 1 
+    questions.pop(question_num)
+    
+    game_length = game_length - 1
+    new_player = switch_player(player_num, int(player_count))
+    player_num = new_player
+
+score_counter = 0
+while score_counter < int(player_count):
+    print ("Player " + str(score_counter+ 1) + " score: " + str(scores[score_counter]))
+    score_counter += 1
