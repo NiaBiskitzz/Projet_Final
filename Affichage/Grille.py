b09451ad57a08e03e76c3a4de1a986361768ca37
Daphnee72 <daphneepelletier18@icloud.com>
2025-04-11 16:08:51 -0400

    Update Grille.py

--- Affichage/Grille.py
+++ Affichage/Grille.py
@@ -1,18 +1,36 @@
 import random
+#faire le case du jeu
 list = ["▢","▢","▢","▢","▢","▢","▢","▢","▢","▢","▢","▢","▢","▢","▢","▢"]
-#mettre 🛇 dans (x,y)
-x = 2
-y = 2
-list[y*4+x] = "🛇"
-#for i in range(4):
-    #print(list[i*4 +0],list[i*4 +1],list[i*4 +2],list[i*4 +3])
-
 def battle_canoe():
-    #Ensuite mettre les bateaux aleatoirement dasn la grille
-    #fonction random
-    bateau = random.randint(0,15)
-    list[bateau] = "🛶"
-
+    for i in range(4):
+        print(list[i*4 +0],list[i*4 +1],list[i*4 +2],list[i*4 +3])
+    #Faire print les case du jeu afin de former un carré 
 battle_canoe()
-for i in range(4):
-    print(list[i*4 +0],list[i*4 +1],list[i*4 +2],list[i*4 +3])
+        #Ensuite mettre les bateaux aleatoirement dasn la grille
+        #fonction random
+
+bateau = (random.randint(0,15), random.randint(0,15))
+bateau = (1,1)
+list[bateau*4+bateau]
+#list[bateau] = "🛶"
+
+x = int(input())
+y = int(input())
+
+while bateau != (x,y):
+
+    #mettre 🛇 dans (x,y)
+    x = int(input())
+    y = int(input())
+    list[y*4+x] = "🛇"
+
+    if bateau == (x,y):
+      print("Reussi")
+    else:
+        print("Raté")
+
+     
+    
+
+
+
