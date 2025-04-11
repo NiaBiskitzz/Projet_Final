e6faaf15ba9b679868d88e75c436a97c95d3dfc2
Grégoire Lemelin <202492084@cegepsherbrooke.qc.ca>
2025-04-11 14:48:41 -0400

    Update décompte.py
    vas juste qua 0

--- Maître_du_temps/Gregoire/décompte.py
+++ Maître_du_temps/Gregoire/décompte.py
@@ -2,7 +2,8 @@ from time import sleep
 
 def decompte():# création de la fonction
     countdown = 5 # notre temps dans ce cas si
-    while countdown > 0: # si le temps n'est pas a 0 le boucle continue
+    while countdown >= 0: # si le temps n'est pas a 0 le boucle continue
         print(countdown)# affiche le temps restant
         sleep(1)# pause de 1 seconde entre chiffre pour rendre sa smooth
         countdown -= 1 # -1 seconde après chaque boucle
+print (decompte())
