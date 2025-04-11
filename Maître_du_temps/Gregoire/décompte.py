e9129b34c1dfc3694e7d9bd411f4ef895783e39e
Grégoire Lemelin <202492084@cegepsherbrooke.qc.ca>
2025-04-11 14:43:26 -0400

    Update décompte.py

--- Maître_du_temps/Gregoire/décompte.py
+++ Maître_du_temps/Gregoire/décompte.py
@@ -1,9 +1,8 @@
 from time import sleep
 
-def decompte():
+def decompte():# création de la fonction
     countdown = 5
     while countdown > 0:
         print(countdown)
         sleep(1)
         countdown -= 1
-print (decompte())
