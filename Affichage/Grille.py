f2d7c4262c3f0627d169dbdb485262defaca0a3e
Daphnee72 <daphneepelletier18@icloud.com>
2025-04-25 14:20:05 -0400

    Update Grille.py
    jeu fini seulement avec 1 joueur par contre je vais travillier pour qui est un deuxieme joueur

--- Affichage/Grille.py
+++ Affichage/Grille.py
@@ -4,30 +4,34 @@ list = ["▢","▢","▢","▢","▢","▢","▢","▢","▢","▢","▢","▢",
 def battle_canoe():
     for i in range(4):
         print(list[i*4 +0],list[i*4 +1],list[i*4 +2],list[i*4 +3])
-    #Faire print les case du jeu afin de former un carré 
+#Faire print les case du jeu afin de former un carré 
 battle_canoe()
-        #Ensuite mettre les bateaux aleatoirement dasn la grille
-        #fonction random
-
+#Ensuite mettre les bateaux aleatoirement dasn la grille
+#fonction random
 bateau = (random.randint(0,15), random.randint(0,15))
 bateau = (1,1)
-list[bateau*4+bateau]
+list[bateau[0]*4+bateau[1]]
 #list[bateau] = "🛶"
 
-x = int(input())
-y = int(input())
+#les varriable de x et y
+x = 0
+y = 1
 
 while bateau != (x,y):
-
-    #mettre 🛇 dans (x,y)
+#mettre 🛇 dans dans la case lorsque na pas la bonne case
     x = int(input())
     y = int(input())
     list[y*4+x] = "🛇"
-#print reussi si sa donne la bonne combinaison 
+#rappler la fonction pour que la grille reappparait avec le signe qui veut dire quon ne la pas eu 
+    battle_canoe()
+    
+    #print reussi et le bateau si sa donne la bonne combinaison et réessayer si pas la bonne combinaison
     if bateau == (x,y):
-      print("Reussi")
+      list[y*4+x] = "🛶" 
+      battle_canoe()
+      print("Bravo!!!")
     else:
-        print("Raté")
+        print("Réessayer")
 
      
     
