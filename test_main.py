887837fa48ce9fc641fd26311b46c620b8b588f9
amelies08 <amelie.sarrazin2@gmail.com>
2025-04-25 16:03:39 -0400

    test main

--- test_main.py
+++ test_main.py
@@ -5,59 +5,66 @@ from Affichage.Effacer import effacer #Permet d'effacer le jeu précédent
 from Affichage.Erreur import game_over #Affichage du gagnant et perdant ainsi que les points gagnés entre les jeux
 from Affichage.point import add #Affiche le nombre de points entre les jeux
 from Maître_du_temps.Gregoire.countdown import countdown
-#from Taper import Taper  #Tape la taupe
-#from black_jack import black_jack #Black jack
-#from duel import duel #Duel
-#from RouletteCasino import roulette #Roulette
+from Battle_canoe import battle_canoe #Battleship
+from trivia2 import trivia_game #Quiz
+from Taper import Taper  #Tape la taupe
+from blackjackV4 import black_jack #Black jack
+from duel import duel #Duel
+from RouletteCasino import roulette() #Roulette
  
 
-pointage_joueur1 = int(1)
-pointage_joueur2 = int(2) 
+pointage_joueur1 = int(0)
+pointage_joueur2 = int(0)
 
 #Démarage du programme des jeux  
 DebutJeu()
-#Sélection du jeu de façon aléatoire
-choix_de_jeu = aléatoire()
-choix_de_jeu = int(choix_de_jeu)
-if choix_de_jeu == 1:
-    print("Battleship") 
-    Réponse_finale.reponsefinale(pointage_joueur1, pointage_joueur2)
-    game_over(Réponse_finale.resultat_mini_jeux)
-    add(Réponse_finale.point_joueur1, Réponse_finale.point_joueur2)
-    countdown()
-    effacer()
-elif choix_de_jeu == 2:
-    print("trivia")
-    Réponse_finale.reponsefinale(pointage_joueur1, pointage_joueur2)
-    game_over(Réponse_finale.resultat_mini_jeux)
-    add(Réponse_finale.point_joueur1, Réponse_finale.point_joueur2)
-    countdown()
-    effacer()
-elif choix_de_jeu == 3:
-    print("Taper")
-    Réponse_finale.reponsefinale(pointage_joueur1, pointage_joueur2)
-    game_over(Réponse_finale.resultat_mini_jeux)
-    add(Réponse_finale.point_joueur1, Réponse_finale.point_joueur2)
-    countdown()
-    effacer()
-elif choix_de_jeu == 4:
-    print ("black_jack")
-    Réponse_finale.reponsefinale(pointage_joueur1, pointage_joueur2)
-    game_over(Réponse_finale.resultat_mini_jeux)
-    add(Réponse_finale.point_joueur1, Réponse_finale.point_joueur2)
-    countdown()
-    effacer()
-elif choix_de_jeu == 5:
-    print("duel")
-    Réponse_finale.reponsefinale(pointage_joueur1, pointage_joueur2)
-    game_over(Réponse_finale.resultat_mini_jeux)
-    add(Réponse_finale.point_joueur1, Réponse_finale.point_joueur2)
-    countdown()
-    effacer()
-else:
-    print("roulette")
-    Réponse_finale.reponsefinale(pointage_joueur1, pointage_joueur2) 
-    game_over(Réponse_finale.resultat_mini_jeux)
-    add(Réponse_finale.point_joueur1, Réponse_finale.point_joueur2)
-    countdown()
-    effacer()
+#Tant que les deux jeux n'ont pas été joués
+while (Réponse_finale.point_joueur1 + Réponse_finale.point_joueur2) != 6:
+    choix_de_jeu = aléatoire() #Choix aléatoire du jeu
+    choix_de_jeu = int(choix_de_jeu)
+    if choix_de_jeu == 1: #Si le choix de jeu est 1
+        battle_canoe() #Le jeu est joué
+        Réponse_finale.reponsefinale(pointage_joueur1, pointage_joueur2) #Les pointages sont calculés en dehors les jeux
+        game_over(Réponse_finale.resultat_mini_jeux) #Afficher quel joueur a gagné et les points gagnés
+        add(Réponse_finale.point_joueur1, Réponse_finale.point_joueur2) #Afficher le nombre de points
+        countdown() #Attendre 15 secondes pour voir l'affichage des points
+        effacer() #Effacer l'affichage des points du jeu
+    elif choix_de_jeu == 2:
+        trivia_game()
+        Réponse_finale.reponsefinale(pointage_joueur1, pointage_joueur2)
+        game_over(Réponse_finale.resultat_mini_jeux)
+        add(Réponse_finale.point_joueur1, Réponse_finale.point_joueur2)
+        countdown()
+        effacer()
+    elif choix_de_jeu == 3:
+        Taper()
+        Réponse_finale.reponsefinale(pointage_joueur1, pointage_joueur2)
+        game_over(Réponse_finale.resultat_mini_jeux)
+        add(Réponse_finale.point_joueur1, Réponse_finale.point_joueur2)
+        countdown()
+        effacer()
+    elif choix_de_jeu == 4:
+        black_jack()
+        Réponse_finale.reponsefinale(pointage_joueur1, pointage_joueur2)
+        game_over(Réponse_finale.resultat_mini_jeux)
+        add(Réponse_finale.point_joueur1, Réponse_finale.point_joueur2)
+        countdown()
+        effacer()
+    elif choix_de_jeu == 5:
+        duel()
+        Réponse_finale.reponsefinale(pointage_joueur1, pointage_joueur2)
+        game_over(Réponse_finale.resultat_mini_jeux)
+        add(Réponse_finale.point_joueur1, Réponse_finale.point_joueur2)
+        countdown()
+        effacer()
+    else:
+        roulette()
+        Réponse_finale.reponsefinale(pointage_joueur1, pointage_joueur2) 
+        game_over(Réponse_finale.resultat_mini_jeux)
+        add(Réponse_finale.point_joueur1, Réponse_finale.point_joueur2)
+        countdown()
+        effacer()
+else: #Lorsque tous les jeux ont été joués
+    print("fin") 
+    
+    
